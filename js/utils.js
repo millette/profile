af7a0d00cb865a9a1b3fda21dbcd28b1a3d28737
Robin Millette <robin@millette.info>
2017-06-13 15:49:49 -0400

    Start on utilities.

--- js/utils.js
+++ js/utils.js
@@ -1,29 +1,17 @@
 'use strict'
 
-Boolean(((w, d) => {
-  const relativeUrl = (o) => {
-    if (!o) { return w.location.href }
-    if (typeof o === 'string') { o = { pathname: o } }
-    if (typeof o !== 'object') { throw new Error('Argument must be a string or an object') }
-    if (!Object.keys(o).length) { return w.location.href }
-    const $a = d.createElement('a')
-    $a.href = w.location.href
-    return Object.assign($a, o).href
+Boolean(((w, d, modName) => {
+  const isBeaker = () => w.navigator.appVersion.indexOf('BeakerBrowser/') !== -1
+
+  const isDat = () => typeof w.DatArchive !== 'undefined' && 'dat:'
+
+  const getProtocol = () => isDat() || w.location.protocol
+
+  const info = () => {
+    console.log('isBeaker:', isBeaker())
+    console.log('isDat:', isDat())
+    console.log('getProtocol:', getProtocol())
   }
 
-/*
- * Example usage:
-
-  const u1 = utils.relativeUrl()
-  const u2 = utils.relativeUrl({pathname: 'dyn-app.json'})
-  const u3 = utils.relativeUrl({pathname: '/dyn-app.json'})
-  const u4 = utils.relativeUrl('dyn-app.json')
-
-  console.log('u1', u1)
-  console.log('u2', u2) // these last 3 have the same output
-  console.log('u3', u3)
-  console.log('u4', u4)
-*/
-
-  w.utils = { relativeUrl }
-})(window, document))
+  w[modName] = { isBeaker, isDat, info }
+})(window, document, 'utils'))
